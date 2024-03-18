bc8329dde71eae2d3412f06fbca2be46b332e262
AbdulhamitSahadi <sahadi.abdulhamit@gmail.com>
2024-03-18 14:42:15 +0300

    Some Updates

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, Anime App.</Text>
+      <Text>Merhaba Mehmet</Text>
       <StatusBar style="auto" />
     </View>
   );
